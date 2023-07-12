081b4f0bd7a37e61f21fac4619b4758acade75d1
Peter Gallagher <gallagherpeter@icloud.com>
2023-07-12 10:06:05 -0400

    using claims now

--- condition.js
+++ condition.js
@@ -1,5 +1,36 @@
 let url = "http://10.10.1.212:9200/logstash-*/_search"
-let data = JSON.stringify({"query":{"match_phrase":{"event_type":"ip_access"}}})
+let data =   {"query": {
+    "bool": {
+      "must": [],
+      "filter": [
+        {
+          "match_all": {}
+        },
+        {
+          "match_phrase": {
+            "event_type": "ip_access"
+          }
+        },
+        {
+          "match_phrase": {
+            "distinguished_name": claims.user.ag.distinguishedName
+          }
+        },
+        {
+          "range": {
+            "@timestamp": {
+              "gte": claims.system.connectTime,
+              "lte": "2023-07-12T13:27:32.879Z",
+              "format": "strict_date_optional_time"
+            }
+          }
+        }
+      ],
+      "should": [],
+      "must_not": []
+    }
+  }
+}
 let contentType = "application/json"
-let response = httpPost(url, data, contentType)
+let response = httpPost(url, JSON.stringify(data), contentType)
 console.log(response.data)
